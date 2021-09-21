279be37d2c3dd236a594d3e61ce18f171d40e41f
Saloni <salonimehta2127@gmail.com>
2021-09-20 20:14:51 -0400

    Completed

--- config/environment.rb
+++ config/environment.rb
@@ -4,3 +4,5 @@ Bundler.require
 require_relative '../lib/song'
 
 DB = { conn: SQLite3::Database.new("db/music.db") }
+# DB is setup as a constant that is equal to a hash that contains our connection to the 
+# database
